bf1962bd377b247dd58412c959e7346a6a6b69b0
cisco <cisco@linux02.mgmt.dovetail-lab.ca>
2022-10-28 11:36:21 -0400

    first

---
+++ 001_main.tf
@@ -0,0 +1,21 @@
+terraform {
+  cloud {
+    hostname     = "terraform.mgmt.dovetail-lab.ca"
+    organization = "DOVETAIL"
+
+
+    workspaces {
+      name = "DOVETAIL"
+    }
+  }
+  required_providers {
+    aci = {
+      source = "ciscodevnet/aci"
+    }
+
+  }
+}
+
+
+
+    
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # DOVETAIL
+
